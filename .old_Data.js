618b6957f4d1ce008f1558ded67f286b6be3725c
rpbritton <mr.ryan.britton@gmail.com>
2017-04-12 13:29:46 -0400

    updated data

---
+++ .old_Data.js
@@ -0,0 +1,146 @@
+﻿var alldata = [
+    {
+        "whenStart": "1919-06-28T04:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Treaty of Versailles",
+        "whatDesc": "The treaty that was forced upon the Germans by the Allies at the end of WW1",
+        "source": "http://www.history.com/topics/world-war-i/treaty-of-versailles",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1920-11-01T05:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "League of Nations Meet",
+        "whatDesc": "The first conference, in which the US was absent, of the LoN in Geneva, Switzerland",
+        "source": "http://www.sparknotes.com/history/european/interwaryears/timeline.html",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1921-11-01T05:00:00.000Z",
+        "whenEnd": "1922-01-01T05:00:00.000Z",
+        "whatTitle": "Washington Conference",
+        "whatDesc": "Countries debated on how to make Europe friendly with eachother and shrink the naval military.",
+        "source": "https://history.state.gov/milestones/1921-1936/naval-conference",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1922-10-30T05:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Mussolini becomes Premier of Italy",
+        "whatDesc": "Benito Mussolini, a facist sympathyzer, gains power.",
+        "source": "http://www.history.com/topics/world-war-ii/benito-mussolini",
+        "picture": "",
+        "pictureSource": "https://www.biography.com/.image/c_fit,cs_srgb,dpr_1.0,q_80,w_620/MTE1ODA0OTcxNzM0MTczMTk3/benito-mussolini-9419443-2-raw.jpg"
+    },
+    {
+        "whenStart": "1923-11-09T05:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Beer Hall Putsch",
+        "whatDesc": "Adolf Hitlers attempted coup of Bavaria's Germany lands him in jail, where he writes Mein Kampf.",
+        "source": "http://www.history.com/topics/beer-hall-putsch#",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1924-01-21T05:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Lenin Dies",
+        "whatDesc": "Russian and Bolshevik leader dies leaving Joseph Stalin to rise into power.",
+        "source": "http://www.history.com/this-day-in-history/vladimir-ilyich-lenin-dies",
+        "picture": "",
+        "pictureSource": "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTIwNjA4NjMzODgyNTEwODYw/vladimir-lenin-9379007-1-402.jpg"
+    },
+    {
+        "whenStart": "1931-09-18T04:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Japan Invades Manchuria",
+        "whatDesc": "The LoN did very little, resulting in people loosing some faith.",
+        "source": "http://www.historylearningsite.co.uk/world-war-two/causes-of-ww2/manchuria-1931/",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1932-02-01T05:00:00.000Z",
+        "whenEnd": "1932-07-01T04:00:00.000Z",
+        "whatTitle": "Last LoN Disarmment Conference",
+        "whatDesc": "Having failed, as always, to come to a disarmment aggreement, the LoN never succeeds.",
+        "source": "http://www.sparknotes.com/history/european/interwaryears/timeline.html",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1933-01-01T05:00:00.000Z",
+        "whenEnd": "1934-01-01T05:00:00.000Z",
+        "whatTitle": "Stalin Purges 1,140,000 Communists",
+        "whatDesc": "Stalin displays his lack of companionship for other parties.",
+        "source": "http://www.historyplace.com/worldhistory/genocide/stalin.htm",
+        "picture": "",
+        "pictureSource": "https://ichef.bbci.co.uk/news/624/cpsprodpb/C18B/production/_87974594_32be803c-efcf-47ef-9a17-197106074016.jpg"
+    },
+    {
+        "whenStart": "1933-01-30T05:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Hitler Becomes Germany's Chancellor",
+        "whatDesc": "As a chance to gain the favor of the Nazi Party, President Paul von Hindenburg gave Adolf Hitler power.",
+        "source": "http://www.history.com/this-day-in-history/adolf-hitler-is-named-chancellor-of-germany",
+        "picture": "",
+        "pictureSource": "https://media1.britannica.com/eb-media/58/129958-004-C9B8B89D.jpg"
+    },
+    {
+        "whenStart": "1933-03-23T05:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Germany's Enabling Act Passes",
+        "whatDesc": "Effectively made Adolf Hitler the dictator of Germany.",
+        "source": "http://www.historyplace.com/worldwar2/timeline/enabling.htm",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1935-10-01T05:00:00.000Z",
+        "whenEnd": "1936-05-01T04:00:00.000Z",
+        "whatTitle": "Italy Invades Abyssinia",
+        "whatDesc": "The LoN again fail to prevent this violence, causing it to loose support of the people and the smaller countries",
+        "source": "http://www.johndclare.net/EL6.htm",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1936-07-17T04:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Spanish Civil War Begins",
+        "whatDesc": "Francisco Franco, Generals Goded, Mola begin a civil war against the republic. ",
+        "source": "http://www.sparknotes.com/history/european/interwaryears/timeline.html",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1938-09-18T04:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Munich Pact Passes",
+        "whatDesc": "Britain and France appease Hitler, letting Germany have Sudetenland.",
+        "source": "http://www.dictionary.com/browse/munich-pact",
+        "picture": "",
+        "pictureSource": ""
+    },
+    {
+        "whenStart": "1939-03-30T05:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Spanish Civil War Ends",
+        "whatDesc": "Francisco Franco wins, resulting in a dictatorship with him at the reins.",
+        "source": "http://www.sparknotes.com/history/european/interwaryears/timeline.html",
+        "picture": "",
+        "pictureSource": "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE5NDg0MDU0OTY4NzYzOTE5/francisco-franco-9300766-2-402.jpg"
+    },
+    {
+        "whenStart": "1939-09-03T04:00:00.000Z",
+        "whenEnd": "",
+        "whatTitle": "Britain and France Declare War on Germany",
+        "whatDesc": "Hitler invaded Poland, leading to Britain and France declaring war on Germany and thus marking the beginning of WW2.",
+        "source": "http://www.history.com/this-day-in-history/britain-and-france-declare-war-on-germany",
+        "picture": "",
+        "pictureSource": ""
+    }
+];
