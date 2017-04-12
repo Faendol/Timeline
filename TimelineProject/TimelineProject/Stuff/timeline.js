e07933aea759f7fb36620a54b88bddad9370dfcf
Faendol <jacquetliam@gmail.com>
2017-04-12 09:51:53 -0400

    Now Functioning WIll Make look better

--- TimelineProject/TimelineProject/Stuff/timeline.js
+++ TimelineProject/TimelineProject/Stuff/timeline.js
@@ -1,5 +1,5 @@
 ﻿var width = window.innerWidth - 40;
-var y = 10;
+var y = 32;
 
 var svg = d3.select("body")
     .append("svg")
@@ -8,6 +8,9 @@ var svg = d3.select("body")
 var first = width * 0.05;
 var second = width * 0.95;
 
+var lines = svg.append("g");
+var circles = svg.append("g");
+var dates = svg.append("g");
 
 //find the largest and smallest date to set the scale
 var maxtime = Date.parse(alldata[0]["whenStart"]);
@@ -37,37 +40,102 @@ var thing = d3.scaleLinear()
     .domain([Date.parse(minyear), Date.parse(maxyear)])
     .range([first, second]);                    
 
+var currentYear = parseInt(minyear);
+var count = 9;
+while (currentYear <= maxyear) {
+    if (count === 9) {
+        lines.append("line")
+            .attr("x1", function () { return thing(Date.parse(currentYear)); })
+            .attr("x2", function () { return thing(Date.parse(currentYear)); })
+            .attr("y1", y - 10)
+            .attr("y2", y + 10)
+            .attr("stroke-width", 5)
+            .attr("stroke", "black");
 
+        dates.append("text")
+            .attr("x", function () { return thing(Date.parse(currentYear)); })
+            .attr("y", y - 11)
+            .attr("text-anchor", "middle")
+            .attr("pointer-events", "none")
+            .text(currentYear);
+        count = -1;
+    }
+    else {
+        lines.append("line")
+            .attr("x1", function () { return thing(Date.parse(currentYear)); })
+            .attr("x2", function () { return thing(Date.parse(currentYear)); })
+            .attr("y1", y - 5)
+            .attr("y2", y + 5)
+            .attr("stroke-width", 5)
+            .attr("stroke", "#424242");
+    }
 
-svg.append("line")
-    .attr("x1", first - 20)
-    .attr("x2", second + 20)
+    count++;
+    currentYear += 1;
+}
+
+lines.append("line")
+    .attr("x1", first)
+    .attr("x2", second)
     .attr("y1", y)
     .attr("y2", y)
     .attr("stroke-width", 5)
     .attr("stroke", "black");
 
-var currentYear = parseInt(minyear);
-while (currentYear <= maxyear) {
-    svg.append("line")
-        .attr("x1", function () { return thing(Date.parse(currentYear)); })
-        .attr("x2", function () { return thing(Date.parse(currentYear)); })
-        .attr("y1", 2)
-        .attr("y2", 18)
-        .attr("stroke-width", 5)
-        .attr("stroke", "black");
-
-    currentYear += 10;
-}
-
-svg.append("g")
+circles
     .selectAll("circle")
     .data(alldata)
     .enter()
+    .append("g")
     .append("circle")
     .attr("cx", function (d, i) { return thing(Date.parse(d["whenStart"])); })
     .attr("cy", y)
     .attr("r", 5)
     .attr("fill", "white")
-    .attr("stroke", "black");
+    .attr("stroke-width", 2)
+    .attr("stroke", "black")
+    .each(function (d, i) {
+        if (i % 2 === 0) {
+            d3.select(this.parentElement)
+                .append("path")
+                .attr("d", "M 34 29 L 34 10 L 30 10 L 30 28 L 24 42 L 40 42")
+                .attr("fill", "#424242")
+                .attr("transform", function () { return "translate(" + (thing(Date.parse(d["whenStart"])) - 32) + "," + (y - 6) + ")"; });
+        }
+        else
+        {
+            d3.select(this.parentElement)
+                .append("path")
+                .attr("d", "M 34 24 L 40 10 L 24 10 L 30 24 L 30 42 L 34 42")
+                .attr("fill", "#424242")
+                .attr("transform", function () { return "translate(" + (thing(Date.parse(d["whenStart"])) - 32) + "," + (y - 46) + ")"; });
+        }
+
+        d3.select(this.parentElement)
+            .on("mouseover", function (d) {
+                document.getElementById("title").innerText = d["whatTitle"];
+                document.getElementById("description").innerText = d["whatDesc"];
+                document.getElementById("source").innerText = d["source"];
+                document.getElementById("date").innerText = d["whenStart"].substring(0, d["whenStart"].indexOf("T"));
+
+                if (d["whenEnd"] !== "") {
+                    document.getElementById("date").innerText += " to " + d["whenEnd"].substring(0, d["whenEnd"].indexOf("T"));
+                }
+
+            })
+            .on("mouseout", function (d) { });
+
+    });
+
+//svg.append("g")
+//    .selectAll("line")
+//    .data(alldata)
+//    .enter()
+//    .append("line")
+//    .attr("x1", function (d, i) { return thing(Date.parse(d["whenStart"])); })
+//    .attr("x2", function (d, i) { return thing(Date.parse(d["whenStart"])); })
+//    .attr("y1", y)
+//    .attr("y2", function (d, i) { return y + 50 + (i * 50);})
+//    .attr("stroke-width", 2)
+//    .style("stroke", "black");
 
